f2e30587416cf44bf5f1305efb88aa18b664bba9
Camden Lupien <143810377+lupes14@users.noreply.github.com>
2025-09-22 12:26:22 -0400

    fix(lsp-config): #11 adjusting lsp-config to not use deprecated lspconfig framework

--- lua/config/lsp-config.lua
+++ lua/config/lsp-config.lua
@@ -2,22 +2,28 @@
 local M = {}
 
 function M.setup()
-  local servers = require("config.mason").setup()
   local lspconfig = require("lspconfig")
   local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  local servers = require("config.mason").setup()
 
-  for _, server in ipairs(servers) do
-    lspconfig[server].setup({
-      capabilities = capabilities,
-      settings = server == "lua_ls" and {
-        Lua = {
-          diagnostics = {
-            globals = { "vim", "Snacks" },
-          },
-        },
-      } or nil,
-    })
-  end
+  require("mason-lspconfig").setup({
+    ensure_installed = servers,
+    handlers = {
+      -- Default handler for all servers
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+          settings = server_name == "lua_ls" and {
+            Lua = {
+              diagnostics = {
+                globals = { "vim", "Snacks" },
+              },
+            },
+          } or nil,
+        })
+      end,
+    },
+  })
 end
 
 return M
